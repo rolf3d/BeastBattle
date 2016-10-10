3f4c518d1ebb79c1b1505eece5ea16f6026ef472
Martin Kierkegaard <mark@easj.dk>
2016-10-10 23:22:17 +0200

    Add project files.

---
+++ Sandbox/BattleLog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    // This class implements a simple logging system, where strings can be collected
+    // one by one. The entire set of strings can then be printed.
+    class BattleLog
+    {
+        List<string> log;
+
+        public BattleLog()
+        {
+            log = new List<string>();
+        }
+
+        // Save a single string
+        public void Save(string message)
+        {
+            log.Add(message);
+        }
+
+        // Print out all the strings saved in the log
+        public void PrintEntireBattleLog()
+        {
+            Console.WriteLine("Battle log :");
+            Console.WriteLine("======================================");
+            foreach (string s in log)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        // Clear out all strings from the log
+        public void Reset()
+        {
+            log.Clear();
+        }
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            NumberGenerator theNumberGenerator = new NumberGenerator();
+            BattleLog theLog = new BattleLog();
+
+            Hero theHero = new Hero(theNumberGenerator, theLog);
+            Beast theBeast = new Beast(theNumberGenerator, theLog);
+
+
+            // Now battle...How do we do that (Hint: You need a loop)
+            // YOUR JOB: Insert code here to make theHero and theBeast battle
+
+
+            theLog.PrintEntireBattleLog();
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
